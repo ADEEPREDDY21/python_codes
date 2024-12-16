ebbfda7694c21ab849ac4031470e630deb893d3e
ADEEP REDDY <adeepreddyvelagala@gmail.com>
2024-12-16 23:38:14 +0530

    Update hw.py

--- hw.py
+++ hw.py
@@ -81,7 +81,42 @@ Determine the amount that will be rebated by the insurance company."""
 for r in range(int(input())):
     x,y=map(int,input().split())
     print(min(x,y))
+"""Chef and Gym
+Chef has decided to join a Gym in ChefLand and if possible, also hire a personal trainer at the gym. The monthly cost of the gym is 
+X
+X and personal training will cost him an additional 
+Y
+Y per month. Chef's total budget per month is only 
+Z
+Z. Print 1 if Chef can only join the gym, 2 if he can also have a personal trainer, and 0 if he can't even join the gym.
 
+Note that if Chef wants to hire a personal trainer, he must join the gym — he cannot hire the trainer without joining the gym.
+
+Input Format
+The first line of input will contain a single integer 
+T
+T, denoting the number of test cases. Then the test cases follow.
+Each test case consists of a single line of input containing three space-separated integers 
+X
+,
+Y
+,
+Z
+X,Y,Z.
+Output Format
+For each test case, output in a single line 2 if Chef can go to the gym and have a trainer, 1 if Chef can only go to the gym, 0 if he can't even go to the gym."""
+def gym_trainer(x,y,z):
+    if x+y<=z:
+        return 2
+    elif x<=z:
+        return 1
+    else:
+        return 0
+t=int(input())
+for r in range(t):
+    x,y,z=map(int,input().split())
+    print(gym_trainer(x,y,z))    
+                    
          
          
 

166e314c0f87fc6e12845e848c68137a7b870628
ADEEP REDDY <adeepreddyvelagala@gmail.com>
2024-12-24 23:47:56 +0530

    Update hw.py

--- hw.py
+++ hw.py
@@ -233,6 +233,18 @@ while len(input_1)>index:
         vowel_c+=1
     index+=1
 print(f"Number of vowels in given string is {vowel_c}") 
+"""A new TV streaming service was recently started in Chefland called the Chef-TV.
+
+A group of N friends in Chefland want to buy Chef-TV subscriptions. We know that 
+6 people can share one Chef-TV subscription. Also, the cost of one Chef-TV subscription is 
+X rupees. Determine the minimum total cost that the group of 
+N friends will incur so that everyone in the group is able to use Chef-TV."""  
+n,x=map(int,input().split())
+numOfFrnds=n#here n is the number of friends willing to take subscription
+costOfSub=x#Cost of subscription to cheftv
+mincost=((n+5//6)*x)
+print(mincost)
+
 
     
 

d94e22f3ecff796aeabb4fb5b0bf804f21b1678f
ADEEP REDDY <adeepreddyvelagala@gmail.com>
2025-01-26 23:16:31 +0530

    Create square_root.py

---
+++ square_root.py
@@ -0,0 +1,9 @@
+# in python we can write code for finding square root of any integers
+import math
+#by importing math library to perform any math related operations
+number=int(input("Enter number to check its square root number:")
+square_root=math.sqrt(n)
+print(f"Square root of {number} is {square_root:.5f}")
+
+           
+             

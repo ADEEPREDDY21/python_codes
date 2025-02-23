d2466dcaf6cfe0ac971912c53bc1f5b857703ed7
ADEEP REDDY <adeepreddyvelagala@gmail.com>
2025-02-23 22:16:01 +0530

    Add files via upload

---
+++ string methods.py
@@ -0,0 +1,9 @@
+#String Methods
+a="Adeep Reddy"
+print("Original String:",a)
+print("\nString Methods:")
+print(dir(a))#List of all string methods
+print("\nCapitalize Methods:")
+print(a.capitalize())
+print("\nReplace Method:")
+print(a.replace("Reddy","The great"))
